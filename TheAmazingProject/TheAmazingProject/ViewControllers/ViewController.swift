65f2322a808887115a58f4e5ec2450a543680347
Leandro Romano <leandroromano.dev@gmail.com>
2020-01-28 21:48:28 -0300

    Changed Makefile directory, generated XCFramework and iOS & Simulator archives and implemented the XCFramework in iOS App.

--- TheAmazingProject/TheAmazingProject/ViewControllers/ViewController.swift
+++ TheAmazingProject/TheAmazingProject/ViewControllers/ViewController.swift
@@ -1,10 +1,18 @@
 import UIKit
+import TheIncredibleFramework
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
+        
+        let timCook = createIncrediblePerson()
+        print(timCook.formattedInformation())
+    }
+    
+    private func createIncrediblePerson() -> IncrediblePerson {
+        return IncrediblePerson(name: "Tim Cook", email: "tcook@apple.com")
     }
 
 }
---
+++ frameworks/TheIncredibleFramework-Simulator.xcarchive/Products/Library/Frameworks/TheIncredibleFramework.framework/Headers/TheIncredibleFramework.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for TheIncredibleFramework.
+FOUNDATION_EXPORT double TheIncredibleFrameworkVersionNumber;
+
+//! Project version string for TheIncredibleFramework.
+FOUNDATION_EXPORT const unsigned char TheIncredibleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TheIncredibleFramework/PublicHeader.h>
+
+
