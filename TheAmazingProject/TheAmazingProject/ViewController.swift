bd0b5380ba476c71163997bff82af8bfa2f69724
Leandro Romano <leandroromano.dev@gmail.com>
2020-01-28 21:15:21 -0300

    Created TheAmazingProject single view app.

---
+++ TheAmazingProject/TheAmazingProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheAmazingProject
+//
+//  Created by Leandro Romano on 28/01/20.
+//  Copyright © 2020 Leandro Romano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
